9b2cd4a90a3b9d103e101c9d6afdc1a18be7d849
ebreitmo <elena@epcc.ed.ac.uk>
2018-02-22 16:46:10 +0000

    starting repo

---
+++ FIPDirectoryReader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Abstractions;
+
+
+
+namespace FIPMessenger
+{
+    class FIPDirectoryReader
+    {
+        static SimpleLogger _logger;
+        static bool consoleOut = true;
+
+        private static List<string> getDicomFileList()
+        {
+            // Searches directories for Dicom files and produces a list of those files
+            var directories = new List<string>();
+            return directories;
+        }
+
+        private static void sendToRabbitMQ(Object doc)
+        {
+            // Sends the given document to the rabbit MQ queue/exchange
+
+        }
+
+        public static List<string> findDicomInDirectoryTree(string directoryRoot)
+        {
+            // Returns a list of directories which contain dicom files
+
+            //TODO(Ruairidh) Check/enforce the expected directory structure?
+            //TODO(Ruairidh) Detect and manage zip / archives etc.
+
+            var dicomDirectories = new List<string>();
+            int totalDicom = 0;
+
+            var dirStack = new Stack<string>();
+            IFileSystem fileSystem = new FileSystem();
+
+            dirStack.Push(directoryRoot);
+
+            while (dirStack.Count > 0)
+            {
+
+                string dir = dirStack.Pop();
+
+                try
+                {
+                    if (fileSystem.Directory.Exists(dir))
+                    {
+
+                        // Add subdirectories to the list of directories to explore
+                        foreach (string subdir in fileSystem.Directory.GetDirectories(dir))
+                        {
+                            dirStack.Push(subdir);
+                        }
+
+                        int nDicomFiles = fileSystem.Directory.EnumerateFiles(dir, "*.dcm").Count();
+
+                        if (nDicomFiles > 0)
+                        {
+                            dicomDirectories.Add(dir);
+                            totalDicom += nDicomFiles;
+                        }
+                    }
+                    else if (fileSystem.File.Exists(dir))
+                    {
+                        Console.WriteLine("That's a file not a directory! (" + dir + ")");
+                    }
+                    else
+                    {
+                        Console.WriteLine("That doesn't exist: " + dir);
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Exception somewhere here");
+                }
+            }
+
+            // Can't use logger here if calling this function from DicomGenerator
+            //if (dicomDirectories.Count > 0)        
+            //_logger.WriteLine(String.Format("Found {0} directories containing {1} dicom files", dicomDirectories.Count, totalDicom), globalSw.Elapsed.TotalSeconds);
+
+            return dicomDirectories;
+        }
+
+
+        static void Main(string[] args)
+        {
+            _logger = new SimpleLogger(@".\logs\", consoleOut);
+            _logger.WriteLine("Starting ...");
+
+            FIPRabbitMQHandler handler = new FIPRabbitMQHandler();
+
+            handler.sendMessage("Hi there for the first time!");
+            string message = handler.receiveMessage();
+            Console.WriteLine("Got message:", message);
+            //FIPDirectoryReader.getDicomFileList();
+
+            _logger.WriteLine("Done ...");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FIPRabbitMQHandler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace FIPMessenger
+{
+    class FIPRabbitMQHandler
+    {
+       
+        
+        private IModel channel_;
+        private IConnection connection_;
+
+
+        public FIPRabbitMQHandler()
+        {
+            // Do initialisation of RabbitMQ
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection_ = factory.CreateConnection();
+            channel_ = connection_.CreateModel();
+
+            // Declare queue
+            channel_.QueueDeclare(
+                    queue: "FIPqTest",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+        }
+        // Create queue (queue parameters)
+
+
+        // Create exchange (exchange parameters)
+
+        // send message(to, message)
+        public void sendMessage(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel_.BasicPublish(exchange: "",
+                                 routingKey: "FIPqTest",
+                                 basicProperties: null,
+                                 body: body);
+        }
+
+        // get message(from, )
+        public string receiveMessage()
+        {
+            var consumer = new EventingBasicConsumer(channel_);
+            string message = "";
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel_.BasicConsume(queue: "FIPqTest",
+                                 autoAck: true,
+                                 consumer: consumer);
+            return message;
+        }
+       
+
+    }
+}
